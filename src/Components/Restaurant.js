1581885aa629256a3b57593159e6729bde465d35
Atharva Dhadke <atharvadhadke2002@gmail.com>
2022-12-14 17:27:30 +0530

    Created a food api and card to display food Items

--- src/Components/Restaurant.js
+++ src/Components/Restaurant.js
@@ -1,9 +1,65 @@
 import React from 'react'
+import "./style.css"
+import Menu from './menuApi'
 
 function Restaurant() {
   return (
     <div>
-      <div className="">Hello</div>
+      <div className="card-container">
+        <div className="card">
+            <div className="card-body">
+                <span className="card-number card-circle subtle">1</span>
+                <span className="card-author subtle">Dinner</span>
+                <h2 className="card-title">Biryani</h2>
+                <span className="card-description subtle">Cooked rice with a flavour of chicken</span>
+                <div className="card-read">Read</div>
+            </div>
+            <img src="" alt="images" className="card-media" />
+            <span className="card-tag subtle"> Order Now</span>
+        </div>
+      </div>
+
+      <div className="card-container">
+        <div className="card">
+            <div className="card-body">
+                <span className="card-number card-circle subtle">1</span>
+                <span className="card-author subtle">Dinner</span>
+                <h2 className="card-title">Biryani</h2>
+                <span className="card-description subtle">Cooked rice with a flavour of chicken</span>
+                <div className="card-read">Read</div>
+            </div>
+            <img src="" alt="images" className="card-media" />
+            <span className="card-tag subtle"> Order Now</span>
+        </div>
+      </div>
+
+      <div className="card-container">
+        <div className="card">
+            <div className="card-body">
+                <span className="card-number card-circle subtle">1</span>
+                <span className="card-author subtle">Dinner</span>
+                <h2 className="card-title">Biryani</h2>
+                <span className="card-description subtle">Cooked rice with a flavour of chicken</span>
+                <div className="card-read">Read</div>
+            </div>
+            <img src="" alt="images" className="card-media" />
+            <span className="card-tag subtle"> Order Now</span>
+        </div>
+      </div>
+
+      <div className="card-container">
+        <div className="card">
+            <div className="card-body">
+                <span className="card-number card-circle subtle">1</span>
+                <span className="card-author subtle">Dinner</span>
+                <h2 className="card-title">Biryani</h2>
+                <span className="card-description subtle">Cooked rice with a flavour of chicken</span>
+                <div className="card-read">Read</div>
+            </div>
+            <img src="" alt="images" className="card-media" />
+            <span className="card-tag subtle"> Order Now</span>
+        </div>
+      </div>
     </div>
   )
 }
